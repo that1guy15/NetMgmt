2e17546b50a13a6bf7ecc3f66d7c326648b650e2
that1guy15 <boothr2@gmail.com>
2017-06-16 17:44:31 -0500

    Add newdevice form and new device creation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from os import listdir
 from os.path import isfile, join
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, SelectField, TextAreaField, SubmitField, BooleanField, validators, ValidationError
 from wtforms.validators import Required
 
