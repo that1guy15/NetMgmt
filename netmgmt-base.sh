b6643105c4c61cdbf581021a823d0882b3128d14
that1guy15 <boothr2@gmail.com>
2017-06-19 18:06:34 -0500

    Initial Version: - Device add

--- netmgmt-base.sh
+++ netmgmt-base.sh
@@ -3,12 +3,12 @@
 echo "Provisioning virtual machine..."
 
 echo "INSTALL GIT"
-    sudo add-apt-repository ppa:git-core/ppa > /dev/null
-    sudo apt-get update > /dev/null
+    sudo add-apt-repository ppa:git-core/ppa
+    sudo apt-get update
     sudo apt-get install git > /dev/null
 
 echo "INSTALL ANSIBLE"
-    sudo apt-add-repository ppa:ansible/ansible > /dev/null
-    sudo apt-get update > /dev/null
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
 	sudo apt-get install ansible > /dev/null
 	
