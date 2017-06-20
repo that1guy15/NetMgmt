830005f1f54306dc64e4ca1926aba2769bff19eb
that1guy15 <boothr2@gmail.com>
2017-06-19 19:29:59 -0500

    Initial Version: - Device add

--- netmgmt-base.sh
+++ netmgmt-base.sh
@@ -6,9 +6,9 @@ echo "*******************************"
 
 echo "ADD APT-GET REPOS"
     sudo su -
-    sudo add-apt-repository ppa:git-core/ppa > /dev/null
-    sudo apt-add-repository ppa:ansible/ansible > /dev/null
-    sudo apt-get update || True
+    sudo add-apt-repository ppa:git-core/ppa -y > /dev/null
+    sudo apt-add-repository ppa:ansible/ansible -y > /dev/null
+    sudo apt-get update
 
 echo "INSTALL GIT"
     sudo apt-get install git > /dev/null
